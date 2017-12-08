9662802f9c30cb4f1583569287e2f94d6f2267c8
Cem Eteke <ceteke13@ku.edu.tr>
2017-12-08 18:20:21 +0300

    logging fix

--- train.py
+++ train.py
@@ -27,14 +27,14 @@ all_losses = []
 train_acc = []
 test_acc = []
 for e in range(FLAGS.epochs):
-  print("Epoch 1:")
+  print("Epoch {}:".format(e+1), flush=True)
   epoch_losses = model.fit(X_train, y_train, FLAGS.batch_size)
-  print("\tMean Loss:", np.mean(epoch_losses))
+  print("\tMean Loss:", np.mean(epoch_losses), flush=True)
   all_losses += epoch_losses
   tr_acc = model.score(X_train, y_train, 200)
-  print("\tTraining Accuracy", tr_acc)
+  print("\tTraining Accuracy", tr_acc, flush=True)
   ts_acc = model.score(X_test, y_test, 200)
-  print("\tTest Accuracy", ts_acc)
+  print("\tTest Accuracy", ts_acc, flush=True)
   train_acc.append(tr_acc)
   test_acc.append(ts_acc)
-  print("===========")
+  print("===========", flush=True)
